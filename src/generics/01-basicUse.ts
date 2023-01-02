604cdef2649ef795537fd28c0f616fa3644ac198
mumu <rmli1912@foxmail.com>
2023-01-02 23:17:51 +0800

    reverse function

--- src/generics/01-basicUse.ts
+++ src/generics/01-basicUse.ts
@@ -10,4 +10,25 @@ const p1: Promise<string> = new Promise((resolve, reject) => {
   }, 2000)
 })
 
-p1.then(data => console.log(data))
+p1.then((data) => console.log(data))
+
+// reverse函数例子
+function reverse<T>(items: T[]): T[] {
+  const toreturn = []
+  for (let i = items.length - 1; i >= 0; i--) {
+    toreturn.push(items[i])
+  }
+  return toreturn
+}
+
+const sample = [1, 2, 3]
+let reversed = reverse(sample)
+
+console.log(reversed) // 3, 2, 1
+
+// Safety
+// reversed[0] = '1'; // Error
+// reversed = ['1', '2']; // Error
+
+reversed[0] = 1 // ok
+reversed = [1, 2] // ok
