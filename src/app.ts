2ea6a3e0cfce4e9462f7f4ac52e53ab044cbecbc
mumu <rmli1912@foxmail.com>
2023-01-05 23:21:57 +0800

    decorator review

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 // TS中装饰器模式
 
 // Decorators 装饰器
+/* 
+  装饰器本质上一种特殊的函数被应用在类上。
+  应用装饰器其实很像是组合一系列函数，类似于高阶函数和类。通过装饰器
+  可以轻松实现代理模式来使得代码更加简洁以及实现其他一些有趣的能力。
 
+  执行
+  装饰器只在解释执行时应用一次
+*/
 
-
-interface ReadonlyFoo {
-  name: string
-  age: number
+// 装饰器的参数多少取决于你在哪里使用装饰器
+function simpleDecorator() {
+  console.log('I am a decorator.')
 }
 
-// type ReadonlyBar = Readonly<ReadonlyFoo>
-
-/*
-  相当于
-  type ReadonlyBar = {
-    readonly name: string
-    readonly age: number
+// 装饰器应该指向一个未执行的函数
+// @simpleDecorator
+class Card {
+  constructor() {
+    console.log("Creating a new card game...")
   }
-*/
+}
