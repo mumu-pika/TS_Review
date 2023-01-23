99b2589bdaaf703360fc65d89103dfd35a6a6937
mumu <rmli1912@foxmail.com>
2023-01-23 23:24:16 +0800

    update: xhr.ts

--- src/ts-axios/src/index.ts
+++ src/ts-axios/src/index.ts
@@ -1,3 +1,3 @@
-import '../types/api'
+import '../types/api' // 引入类型声明文件
 function axios(config: api.AxiosRequestConfig) {}
 export default axios
---
+++ src/ts-axios/src/xhr.ts
@@ -0,0 +1,4 @@
+// 利用XMLHttpRequest发送请求
+import '../types/api' // 引入类型声明文件
+
+export default function xhr(config: api.AxiosRequestConfig) {}
